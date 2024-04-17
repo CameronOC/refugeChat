e7bd931b3bf359cd2cb555b65fd6971bb96004d0
CameronOC <cjplanes@gmail.com>
2024-04-16 22:48:09 -0400

    Added commands for bot to run and send command help info on an interval

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 const tmi = require('tmi.js');
-const oauth = require('./auth.js')
+const oauth = require('./auth.js');
+// import { REST, Routes } from 'discord.js';
+
 // Define configuration options
 const opts = oauth.opts;
 
@@ -13,6 +15,17 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+// Comands for the bot 
+const sendCommands =  (data) => {
+  let date = new Date();
+  const minutes = date.getMinutes();
+  if(minutes === 0){
+    client.say(opts.channels[0], getDJ);
+  } else if(minutes % 10 === 0){
+    client.say(opts.channels[0], `To get the current dj send !dj`);
+  }
+};
+
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
@@ -23,20 +36,22 @@ function onMessageHandler (target, context, msg, self) {
 
   // If the command is known, let's execute it
   if (commandName === '!dj') {
-    const dj = getDJ();
-    client.say(target, `The current DJ is: ${dj}`);
+    client.say(target, getDJ);
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
 }
 
-// Function called when the "dice" command is issued
+// Function called when the "dj" command is issued
 function getDJ () {
-  return '<Current DJ>'
+  let dj = 'dj';
+  return `The current DJ is: ${dj}`
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
+  console.log(`Beginning sendComands info for twitch enjoyers`);
+  setInterval(sendCommands, 60000);
 }
